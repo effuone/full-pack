e471740aa3e8b80e3e475eebd46b3d8f1ed0a398
effuone <effuone>
2022-08-13 16:15:04 +0600

    navbar updated

--- src/components/common/navbar/Header.jsx
+++ src/components/common/navbar/Header.jsx
@@ -1,31 +1,43 @@
 import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark py-3">
-        <div className="container">
-           <Nav.Link as={Link} to="/about" className="navbar-brand text-dark">Fullpack</Nav.Link>
-           <button 
-              className="navbar-toggler" 
-              type="button" 
-              data-bs-toggle="collapse" 
-              data-bs-target="#navmenu"
-              >
-           <span className="navbar-toggler-icon"></span>
-           </button>
-           <div className="collapse navbar-collapse">
-              <ul className="navbar-nav">
-                 <li className="nav-item">
-                     <Nav.Link as={Link} to="/about" className="text-dark">О проекте</Nav.Link>
-                 </li>
-                 <li className="nav-ites">
-                     <Nav.Link as={Link} to="/categories" className="text-dark">Категории</Nav.Link>
-                 </li>
-              </ul>
-           </div>
-        </div>
-     </nav>
+   //      <Navbar className="navbar navbar-expand-lg navbar-dark py-3">
+   //      <div className="container">
+   //         <Nav.Link as={Link} to="/about" className="navbar-brand text-dark">Fullpack</Nav.Link>
+   //         <button 
+   //            className="navbar-toggler second-button" 
+   //            type="button"
+   //            data-toggle="collapse"
+   //            data-target="#navmenu"
+   //            >
+   //         <span className="navbar-toggler-icon"></span>
+   //         </button>
+   //         <div className="collapse navbar-collapse" id="navmenu">
+   //            <ul className="navbar-nav">
+   //               <li className="nav-item">
+   //                   <Nav.Link as={Link} to="/about" className="text-dark">О проекте</Nav.Link>
+   //               </li>
+   //               <li className="nav-ites">
+   //                   <Nav.Link as={Link} to="/categories" className="text-dark">Категории</Nav.Link>
+   //               </li>
+   //            </ul>
+   //         </div>
+   //      </div>
+   //   </Navbar>
+      <Navbar bg="none" expand="lg">
+         <Container>
+            <Navbar.Brand as={Link} to="/about">FullPack</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+               <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/about" className="text-dark">О проекте</Nav.Link>
+                  <Nav.Link as={Link} to="/categories" className="text-dark">Категории</Nav.Link>
+               </Nav>
+            </Navbar.Collapse>
+         </Container>
+      </Navbar>
     )
 }
 export default Header
